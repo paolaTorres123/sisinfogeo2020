58865ed1489c16a6c9b61748cbec17ef08adf30e
paolaTorres123 <pao.paotorres18@gmail.com>
2020-01-30 11:38:52 -0600

    Area del circulo

---
+++ 02.areaCirculo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.areaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio,area;
+
+            Console.WriteLine("Programa que calcula el area de un circulo");
+
+            Console.WriteLine("Dame el radio ");
+            radio = double.Parse(Console.ReadLine());
+
+            area = Math.PI * Math.Pow(radio,2);
+            Console.WriteLine($"El area del circulo es:{area}");
+            Console.ReadLine();
+        }
+    }
+}
