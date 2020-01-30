bc585b1b4ec68a299e06f451d2e3d0e6148a2600
paolaTorres123 <pao.paotorres18@gmail.com>
2020-01-30 11:20:09 -0600

    Programa hola mundo

---
+++ 01.holamundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01.holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	    Console.WriteLine("bIENVENIDO!");
+        }
+    }
+}
