a587b0c987c51335931dcf966039d7cb3a28fb46
paolatores <pao.paotores18@gmail.com>
2020-04-30 20:28:31 -0500

    PrimerEjemplo de Interfaces

---
+++ 25.Interfaz1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _25.Interfaz1
+{
+    class Program
+    {
+        public interface IAnimal
+        {
+            string Nombre {get; set;}
+            void Llorar();
+        }
+        class Perro : IAnimal
+        {
+            public Perro (string nombre) => Nombre=nombre;
+            public string Nombre {get; set;}
+            public void Llorar() => Console.WriteLine("Woff Woff");
+
+        }
+        class Gato : IAnimal
+        {
+            public Gato(string nombre) => Nombre = nombre;
+            public string Nombre {get; set;}
+            public void Llorar() => Console.WriteLine("Meow Meow");
+        }
+        static void Main(string[] args)
+        {
+            Perro perro = new Perro("Sabueso");
+            Console.WriteLine($"El perro {perro.Nombre}");
+            perro.Llorar();
+
+            Gato gato = new Gato("Mishifus");
+            Console.WriteLine($"El gato {gato.Nombre}");
+            gato.Llorar();
+        }
+    }
+}
